5707959613f6e829862980f51cc45079504f71d8
Antonio-Icaro <antonio.icaro@aluno.uece.br>
2025-11-23 13:01:43 -0300

    Funções adicionadas na classe DiGraph, faço testes futuramente com essas funções, itens feitos são a, b, c,e, g, i, k

--- Grafoteca.py
+++ Grafoteca.py
@@ -1,19 +1,14 @@
-global time
-time=0
-
-global predecessors
-predecessors={}*None
-
-global visited
-visited={}*False
-
-global init_time, finish_time
-init_time=[]
-finish_time=[]
+import heapq
 
 class Graph:
     def __init__(self,adj_list):
-        self.adj = adj_list
+        vertices = set(adj_list.keys())
+        self.adj = {v: adj_list.get(v, {}) for v in vertices}
+        self.time = 0
+        self.visited = {v: False for v in self.adj}
+        self.predecessors = {v: None for v in self.adj}
+        self.init_time = {v: None for v in self.adj}
+        self.finish_time = {v: None for v in self.adj}
     
     def count_vertexs(self):
         vertex=set()
@@ -49,21 +44,140 @@ class Graph:
         print("O vértice de maior grau é:",vertex,"com grau",edges)  
     
     def dfs(self,v,parent=None):
-        init_time[v]=time + 1
-        visited[v]=True
-        predecessors[v]=parent
+        self.time = 0
+        self.visited = {vertex: False for vertex in self.adj}
+        self.predecessors = {vertex: None for vertex in self.adj}
+        self.init_time = {vertex: None for vertex in self.adj}
+        self.finish_time = {vertex: None for vertex in self.adj}
+        
+        self.time += 1
+        self.init_time[v]=self.time
+        self.visited[v]=True
+        self.predecessors[v]=parent
         
         for u in self.adj[v]:
-            if not visited[u]:
+            if not self.visited[u]:
                 self.dfs(u,v)
-        finish_time[v]= time + 1
+        self.time += 1
+        self.finish_time[v]= self.time 
     
         return {
-        init_time,
-        finish_time,
-        predecessors
+           "init_time":self.init_time,
+           "finish_time":self.finish_time,
+           "predecessors":self.predecessors
     }        
         
+    def dijkstra(self,src):
+        initial_distance= float('inf')
+        dist={v: initial_distance for v in self.adj}
+        dist[src]=0
+        pq=[(0,src)]
+        while pq:
+            d ,u = heapq.heappop(pq)
+            if d>dist[u]:
+                continue
+            else:
+                for v in self.adj[u]:
+                    w= self.adj[u][v]
+                    if dist[u]+w<dist[v]:
+                        dist[v]=dist[u]+w
+                        heapq.heappush(pq,(dist[v],v))
+        return dist
+
 class DiGraph:
-    def __init__(self,adj_list):
-        self.adj = adj_list
+    def __init__(self, adj_list):
+        vertex = set(adj_list.keys())
+        self.adj = {v: adj_list.get(v, {}) for v in vertex}
+        self.time = 0
+        self.visited = {v: False for v in self.adj}
+        self.predecessors = {v: None for v in self.adj}
+        self.init_time = {v: None for v in self.adj}
+        self.finish_time = {v: None for v in self.adj}
+    
+    def count_vertexs(self):
+        vertex = set()
+        for a in self.adj:
+            vertex.add(a)
+        print("O número de vértices é:", len(vertex))
+    
+    def count_edges(self):
+        edges = 0
+        seen_edges = set()  
+        for u in self.adj:
+            for v in self.adj[u]:
+                edge = (u, v)
+                if edge not in seen_edges:
+                    seen_edges.add(edge)
+                    edges += 1
+        print("O número de arcos (arestas direcionadas) é:", edges)
+    
+    def see_neighbors(self, v):
+        if v in self.adj:
+            return self.adj[v]
+        else:
+            return "Vértice não encontrado"
+    
+    def see_edge_weight(self, u, v):
+        if u in self.adj and v in self.adj[u]:
+            return self.adj[u][v]
+        else:
+            return "Arco não encontrado"
+    
+    def see_biggest(self):
+
+        out_degree = {v: len(self.adj.get(v, {})) for v in self.adj}
+
+        in_degree = {v: 0 for v in self.adj}
+        for u in self.adj:
+            for v in self.adj[u]:
+                in_degree[v] += 1
+        
+        total_degree = {v: out_degree[v] + in_degree[v] for v in self.adj}
+        max_degree = -1
+        vertex = None
+        for v in total_degree:
+            if total_degree[v] > max_degree:
+                max_degree = total_degree[v]
+                vertex = v
+        print(f"O vértice de maior grau total é: {vertex}, com grau {max_degree} (in-degree: {in_degree[vertex]}, out-degree: {out_degree[vertex]})")
+    
+    def dfs(self, v, parent=None):
+        self.time = 0
+        self.visited = {vertex: False for vertex in self.adj}
+        self.predecessors = {vertex: None for vertex in self.adj}
+        self.init_time = {vertex: None for vertex in self.adj}
+        self.finish_time = {vertex: None for vertex in self.adj}
+        
+        self.time += 1
+        self.init_time[v] = self.time
+        self.visited[v] = True
+        self.predecessors[v] = parent
+        
+        for u in self.adj[v]:
+            if not self.visited[u]:
+                self.dfs(u, v)
+        self.time += 1
+        self.finish_time[v] = self.time 
+        
+        return {
+            "init_time": self.init_time,
+            "finish_time": self.finish_time,
+            "predecessors": self.predecessors
+        }
+        
+    def dijkstra(self, src):
+        initial_distance = float('inf')
+        dist = {v: initial_distance for v in self.adj}
+        dist[src] = 0
+        pq = [(0, src)]
+        while pq:
+            d, u = heapq.heappop(pq)
+            if d > dist[u]:
+                continue
+            else:
+                for v in self.adj[u]:
+                    w = self.adj[u][v]
+                    if dist[u] + w < dist[v]:
+                        dist[v] = dist[u] + w
+                        heapq.heappush(pq, (dist[v], v))
+        return dist
