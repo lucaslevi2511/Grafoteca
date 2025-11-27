a17d5ff58786af41ec5509da931e6e07e60efaa8
Davi Emanuel de Medeiros <luclevi0102@gmail.com>
2025-11-27 07:47:53 -0300

    Todas as funções até bfs  renomeadas e implementadas em ambas as classes

--- Grafoteca.py
+++ Grafoteca.py
@@ -127,13 +127,13 @@ class DiGraph:
         self.init_time = {v: None for v in self.adj}
         self.finish_time = {v: None for v in self.adj}
     
-    def count_vertexs(self):
+    def n(self):
         vertex = set()
         for a in self.adj:
             vertex.add(a)
         print("O número de vértices é:", len(vertex))
     
-    def count_edges(self):
+    def m(self):
         edges = 0
         seen_edges = set()  
         for u in self.adj:
@@ -144,19 +144,33 @@ class DiGraph:
                     edges += 1
         print("O número de arcos (arestas direcionadas) é:", edges)
     
-    def see_neighbors(self, v):
+    def v(self, v):
         if v in self.adj:
             return self.adj[v]
         else:
             return "Vértice não encontrado"
-    
-    def see_edge_weight(self, u, v):
+
+    def w(self, u, v):
         if u in self.adj and v in self.adj[u]:
             return self.adj[u][v]
         else:
             return "Arco não encontrado"
     
-    def see_biggest(self):
+    def mind(self):
+        out_degree = {v: len(self.adj[v] for v in self.ad)}
+        in_degree = {v: 0 for v in self.adj}
+
+        for u in self.adj:
+            for v in self.adj[u]:
+                in_degree[v] += 1
+        
+        total_degree = {v: out_degree[v] + in_degree for v in self.adj}
+        min_v = min(total_degree, key=lambda x: total_degree[x]) 
+        min_degree = total_degree[min_degree]
+
+        print(f"O vértice de menor grau total é: {min_v}, com grau {min_degree} (in-degree: {in_degree[min_v]}, out-degree: {out_degree[min_v]})")
+    
+    def maxd(self):
 
         out_degree = {v: len(self.adj.get(v, {})) for v in self.adj}
 
@@ -174,6 +188,25 @@ class DiGraph:
                 vertex = v
         print(f"O vértice de maior grau total é: {vertex}, com grau {max_degree} (in-degree: {in_degree[vertex]}, out-degree: {out_degree[vertex]})")
     
+    def bfs (self,s):
+        dist = {v: float ('inf' for v in self.adj)}
+
+        pi = {v: None for v in self.adj}
+
+        queue = deque()
+        dist [s] = 0
+        queue.append(s)
+
+        while queue:
+            u = queue.popleft()
+            for v in self.adj[u]:
+                if dist == 'inf':
+                    dist[v] = dist[u] + 1
+                    pi[v] = u 
+                    queue.append(v)
+        
+        return dist, pi
+
     def dfs(self, v, parent=None):
         self.time = 0
         self.visited = {vertex: False for vertex in self.adj}
